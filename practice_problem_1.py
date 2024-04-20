63a0eeee5ad5bcb89ca5042740439bb2dcd59802
Dwammers <soundcheck63@gmail.com>
2024-04-20 15:42:10 -0400

    Update practice_problem_1.py

--- practice_problem_1.py
+++ practice_problem_1.py
@@ -2,3 +2,12 @@
 # If the number is divisible by 3, instead print 'Fizz'
 # If the number is divisible by 5, instead print 'Buzz'
 # If the number is divisible by both 3 and 5, print 'FizzBuzz'
+
+for i in range(100):
+    print(i)
+    if i % 3 == 0:
+        print("Fizz")
+        if i % 5 == 0:
+            print("Buzz")
+            if i % 3 == 0 and i % 5 == 0:
+                print("FizzBuzz")
