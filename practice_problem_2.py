3c8efd665a1b68ff4c1b51d122baf33e34107576
Mark Chae <mchae067@gmail.com>
2024-04-21 00:23:35 -0400

    Adding some more problems for practice

--- practice_problem_2.py
+++ practice_problem_2.py
@@ -1,3 +1,7 @@
 # Given an input of nickels, pennies and a goal total, return True if the
-# exact total can be made using the given nickels and pennis.
+# exact total can be made using the given nickels and pennies.
 # Extra coins are ok.
+
+# Repeat the above, but with an input of a goal total, pennies, nickels, and dimes.
+
+# Once again, but with an input of a goal total, pennies, nickels, dimes, and quarters.
---
+++ practice_problem_4.py
@@ -0,0 +1,10 @@
+# Your character can make small jumps and big jumps.
+# Small jumps are equal to 1 unit while big jumps are equal to 7.
+# Write a program to calculate the number of small and big jumps needed
+# to scale a Flag of height 100.
+
+# Again, but a Flag of height 10,000
+
+# Write a function with an input Flag height and a big jump height.
+# Calculate the total number of jumps (small and big combined) 
+# needed to reach the flag. A small jump is equal to 1 unit
