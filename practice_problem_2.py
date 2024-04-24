c88bf3454992b1921a2ffdcab900793ab71b7f68
Dwammers <soundcheck63@gmail.com>
2024-04-24 16:29:03 -0400

    Update practice_problem_2.py

--- practice_problem_2.py
+++ practice_problem_2.py
@@ -2,6 +2,17 @@
 # exact total can be made using the given nickels and pennies.
 # Extra coins are ok.
 
+
+
+def Coin(n):
+    for i in range(n+1):
+        if i % 5 == 0:
+            print("nickel")
+        elif i % 1 == 0:
+            print("penny")
+
+Coin(10)
+
 # Repeat the above, but with an input of a goal total, pennies, nickels, and dimes.
 
 # Once again, but with an input of a goal total, pennies, nickels, dimes, and quarters.
