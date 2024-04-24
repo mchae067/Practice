ab435a5544e51dc06b9485f414a0ac2c069adebe
Mark Chae <mchae067@gmail.com>
2024-04-23 20:51:28 -0400

    Update practice_problem_3.py

--- practice_problem_3.py
+++ practice_problem_3.py
@@ -1,7 +1,4 @@
 # Display the fibonacci sequence up to 10 digits
-
-# Given an input, display the fibonacci sequence up to n digits
-
 do = 0
 re = 1
 print(do)
@@ -11,3 +8,30 @@ for i in range(8):
     fa = do + re
     do = re
     re = fa
+
+
+# Given an input, display the fibonacci sequence up to n digits
+def fibonacci(n):
+    n_minus_2 = 0
+    n_minus_1 = 1
+    if n == 0:
+        print("Nothing! You get nothing!")
+    elif n == 1:
+        print(n_minus_2)
+    elif n == 2:
+        print(n_minus_2)
+        print(n_minus_1)
+    else:
+        print(n_minus_2)
+        print(n_minus_1)
+        for i in range(n-2):
+            print(n_minus_2 + n_minus_1)
+            sum = n_minus_2 + n_minus_1
+            n_minus_2 = n_minus_1
+            n_minus_1 = sum
+
+
+fibonacci(10)
+fibonacci(0)
+fibonacci(1)
+fibonacci(2)
