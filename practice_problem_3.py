80f9cc1ca81e9e0017d93c85aefcc5dda7a48de6
Dwammers <soundcheck63@gmail.com>
2024-04-23 20:00:50 -0400

    Update practice_problem_3.py

--- practice_problem_3.py
+++ practice_problem_3.py
@@ -1,3 +1,13 @@
 # Display the fibonacci sequence up to 10 digits
 
 # Given an input, display the fibonacci sequence up to n digits
+
+do = 0
+re = 1
+print(do)
+print(re)
+for i in range(8):
+    print(do + re)
+    fa = do + re
+    do = re
+    re = fa
