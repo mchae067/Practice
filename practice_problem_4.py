cf0b45938a30073fa2218866969f2b7bcac1a4b3
Mark Chae <mchae067@gmail.com>
2024-04-24 17:17:34 -0400

    Add template and examples for problem 4

--- practice_problem_4.py
+++ practice_problem_4.py
@@ -8,3 +8,9 @@
 # Write a function with an input Flag height and a big jump height.
 # Calculate the total number of jumps (small and big combined) 
 # needed to reach the flag. A small jump is equal to 1 unit
+def num_jumps(flag_height, big_jump):
+    return
+
+print(num_jumps(100, 10))  # Should print 0, 10
+print(num_jumps(100, 9))  # Should print 1, 11
+print(num_jumps(10, 11))  # Should print 10, 0
